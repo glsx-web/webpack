e5a26576fc733be49be8739236222fc68106d20a
liimin <17653212@qq.com>
2018-07-14 02:47:43 +0800

    模版调整

--- template/src/store/index.js
+++ template/src/store/index.js
@@ -2,15 +2,15 @@
  * @Author: limin
  * @Date: 2018-06-25 10:29:36
  * @Last Modified by: limin
- * @Last Modified time: 2018-07-13 17:32:12
+ * @Last Modified time: 2018-07-14 02:46:52
  */
 import Vue from 'vue'
 import Vuex from 'vuex'
 Vue.use(Vuex)
-import { GlAppStore } from 'glsx-vue-admin'
+import { storeModules } from './index'
 const store = new Vuex.Store({
   modules: {
-    ...GlAppStore
+    ...storeModules
   }
 })
 export default store
