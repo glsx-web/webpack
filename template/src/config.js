355ba708b6caee5939b9a02436650bdadabfecc2
cwq0219 <2838673010@qq.com>
2018-09-06 09:40:37 +0800

    修改config

--- template/src/config.js
+++
@@ -1,9 +0,0 @@
-export default{
-  app: {
-    axios: {
-      baseURL: 'http://192.168.3.171:7300/mock/5b0ed7dbf189006180803286/standard',
-      'whiteList': ['user/login', 'user/info', 'user/logout', 'user/getInfo'],
-      'timeout': 15000
-    }
-  }
-}
