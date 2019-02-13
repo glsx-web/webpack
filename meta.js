33aec452cdad3ab8d1cd3eb787b3ac0efd7c1140
cwq0219 <cwqnn2015@163.com>
2019-02-13 11:20:46 +0800

    meta.js-filters-router删掉

--- meta.js
+++ meta.js
@@ -169,12 +169,12 @@ module.exports = {
     'build/webpack.test.conf.js': "unit && runner === 'karma'",
     'test/unit/**/*': 'unit',
     'test/unit/index.js': "unit && runner === 'karma'",
-    // 'test/unit/jest.conf.js': "unit && runner === 'jest'",
+    'test/unit/jest.conf.js': "unit && runner === 'jest'",
     'test/unit/karma.conf.js': "unit && runner === 'karma'",
     'test/unit/specs/index.js': "unit && runner === 'karma'",
     'test/unit/setup.js': "unit && runner === 'jest'",
     'test/e2e/**/*': 'e2e',
-    'src/router/**/*': 'router',
+    // 'src/router/**/*': 'router',
   },
   complete: function(data, { chalk }) {
     const green = chalk.green
