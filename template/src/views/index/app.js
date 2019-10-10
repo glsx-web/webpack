f169854fca6f512a4b7c46d88936145268f09a5b
cwq0219 <cwqnn2015@163.com>
2019-10-10 16:19:17 +0800

    修改

---
+++ template/src/views/index/app.js
@@ -0,0 +1,9 @@
+import App from './app.vue'
+import base from '@/base'
+import GlsxVueCommon from 'glsx-vue-common'
+
+base.getConfig().then(data => {
+  const { vue, config } = data
+  vue.use(GlsxVueCommon, config)
+  base.render(App)
+})
