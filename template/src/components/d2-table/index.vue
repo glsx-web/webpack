2b57fe95cdd514c658955643666a0a13188c53f8
cwq0219 <cwqnn2015@163.com>
2019-10-10 11:32:14 +0800

    修改

--- template/src/components/d2-table/index.vue
+++ template/src/components/d2-table/index.vue
@@ -25,7 +25,7 @@
         :align="item.align||'center'"
       >
         <template slot-scope="scope">
-          <template v-if="item.tagLabel">
+          <!-- <template v-if="item.tagLabel">
             <el-tag v-if="!item.tagName"
               :type="tableParams.tag.type[scope.row[item.prop]]"
               >{{tableParams.tag.label[scope.row[item.prop]]}}
@@ -34,8 +34,8 @@
               :type="tableParams[item.tagName].type[scope.row[item.prop]]"
               >{{tableParams[item.tagName].label[scope.row[item.prop]]}}
             </el-tag>
-          </template>
-          <template v-else-if="item.formatter">
+          </template> -->
+          <template v-if="item.formatter">
             {{item.formatter(scope.row)}}
           </template>
           <template v-else-if="getListApi">
