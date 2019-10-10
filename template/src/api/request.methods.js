6c4163ba3405318a26a8b7685f06204d5350d026
cwq0219 <cwqnn2015@163.com>
2019-10-10 16:27:12 +0800

    修改

---
+++ template/src/api/request.methods.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: limin
+ * @Date: 2019-08-26 18:01:59
+ * @Last Modified by: limin
+ * @Last Modified time: 2019-08-26 18:02:32
+ */
+const RequestMethods = {
+  POST: 'post',
+  GET: 'get',
+  PUT: 'put',
+  DELETE: 'delete'
+}
+export default RequestMethods
