854f43a1f05bb24a7b4a6203214130ce2d15559b
cwq0219 <cwqnn2015@163.com>
2019-10-11 15:34:42 +0800

    ee

---
+++ template/src/views/customer/glsx/index.vue
@@ -0,0 +1,17 @@
+<!--  -->
+<template>
+  <div>
+    <a href="http://www.glsx.com.cn/">广联赛讯主页</a>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
