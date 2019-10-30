045bb20557cfe6c9f55a0557db95854b59243c26
cwq0219 <cwqnn2015@163.com>
2019-10-30 18:06:37 +0800

    修改

--- template/src/components/d2-table/index.vue
+++ template/src/components/d2-table/index.vue
@@ -30,12 +30,12 @@
             <el-tag
               v-if="!item.tagName"
               :type="tableParams.tag.type[scope.row[item.prop]]"
-            >{{tableParams.tag.label[+scope.row[item.prop]]}}
+            >{{tableParams.tag['label'][scope.row[item.prop]]}}
             </el-tag>
             <el-tag
               v-else
               :type="tableParams[item.tagName].type[scope.row[item.prop]]"
-            >{{tableParams[item.tagName].label[+scope.row[item.prop]]}}
+            >{{tableParams[item.tagName].label[scope.row[item.prop]]}}
             </el-tag>
             /* eslint-enable no-alert, no-console */
           </template>
