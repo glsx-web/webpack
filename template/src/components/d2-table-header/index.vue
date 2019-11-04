92b9d9dd750974e9849f4e2cdb66adbbaae6e0f3
cwq0219 <cwqnn2015@163.com>
2019-11-04 17:25:57 +0800

    修改

--- template/src/components/d2-table-header/index.vue
+++ template/src/components/d2-table-header/index.vue
@@ -1,8 +1,8 @@
 <template>
   <el-row class="table-header">
     <el-col :span="12">
-      <d2-el-icon name="s-grid" />
-      <span v-text="title"></span>
+      <d2-el-icon name="s-grid"></d2-el-icon>
+       {{title}}
     </el-col>
     <el-col :span="12">
       <slot></slot>
