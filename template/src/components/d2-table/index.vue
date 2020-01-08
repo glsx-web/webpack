a2ea4e797c421f04e2c23a4bc380fcabdbd161ee
cwq0219 <cwqnn2015@163.com>
2020-01-08 17:38:04 +0800

    修改

--- template/src/components/d2-table/index.vue
+++ template/src/components/d2-table/index.vue
@@ -35,7 +35,8 @@
             :momFlag="item.momFlag"
           ></slot>
           <template v-else-if="item.momFlag">
-            <div v-text="scope.row[item.prop]">
+            <div>
+              <span v-text="scope.row[item.prop]"></span>
               <span
                 v-if="scope.row[item.momFlag] > 0"
                 class="el-icon-top"
@@ -60,7 +61,9 @@
               v-text="tableParams[item.tagName].label[scope.row[item.prop]]"
             ></el-tag>
           </template>
-          <template v-else-if="item.formatter" v-text="item.formatter(scope.row)"></template>
+          <template v-else-if="item.formatter">
+            <span v-text="item.formatter(scope.row)"></span>
+          </template>
           <template v-else-if="getListApi">
             <p v-text="data[scope.$index][item.prop]"></p>
           </template>
