bf1d53a153096e61d52b80b64eb391441f1d4e21
cwq0219 <cwqnn2015@163.com>
2020-01-10 09:54:40 +0800

    修改请求地址

--- template/src/main.js
+++ template/src/main.js
@@ -28,7 +28,7 @@ import router from './router'
 
 /* eslint-disable no-new */
 (async () => {
-  const URL = 'http://oss-config.test.glsx.com.cn/mock/5be17454f31545347559d499/config'
+  const URL = 'https://oss-config.test.glsx.com.cn/mock/5be17454f31545347559d499/config'
   const admin = await axios.get(URL + '/admin')
   const common = await axios.get(URL + '/admin_common')
   Vue.use(GlsxVueComponets)
